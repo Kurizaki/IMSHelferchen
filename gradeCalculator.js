5f1452f4c1e794795d23a36de696a20904d42331
Timog <110891995+timog06@users.noreply.github.com>
2023-12-31 15:10:58 +0100

    gradecalc works again

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -5,34 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
   gradeCalculatorSection.appendChild(gradesContainer);
 
   function updateMedian() {
-    const gradeValues = gradesContainer
-      .querySelectorAll(".grade-input")
-      .map((input) => parseFloat(input.value) || 0)
-      .filter((value) => value !== 0)
+    const gradeValues = Array.from(gradesContainer.querySelectorAll(".grade-input"))
+      .map(input => parseFloat(input.value) || 0)
+      .filter(value => value !== 0)
       .sort((a, b) => a - b);
 
     let median = 0;
     const mid = Math.floor(gradeValues.length / 2);
 
     if (gradeValues.length > 0) {
-      median =
-        gradeValues.length % 2 !== 0
-          ? gradeValues[mid]
-          : (gradeValues[mid - 1] + gradeValues[mid]) / 2;
+      median = gradeValues.length % 2 !== 0
+        ? gradeValues[mid]
+        : (gradeValues[mid - 1] + gradeValues[mid]) / 2;
     }
 
     document.getElementById("median-grade").textContent = median.toFixed(2);
   }
-  // Function to update the average grade
+
   function updateAverage() {
     const gradeInputs = gradesContainer.querySelectorAll(".grade-input");
     const weightInputs = gradesContainer.querySelectorAll(".weight-input");
-    let total = 0,
-      weightSum = 0;
+    let total = 0, weightSum = 0;
 
     gradeInputs.forEach((gradeInput, index) => {
       const grade = parseFloat(gradeInput.value) || 0;
-      const weight = parseFloat(weightInputs[index].value) || 0;
+      const weight = parseFloat(weightInputs[index].value) || 1;
       if (grade <= 7 && grade >= 1) {
         total += grade * weight;
         weightSum += weight;
@@ -50,26 +47,14 @@ document.addEventListener("DOMContentLoaded", () => {
       updateAverage();
     }
   }
-  function toggleGradeCalculator() {
-    if (gradeCalculatorSection.style.display === "none") {
-      gradeCalculatorSection.style.display = "block";
-      console.log("claculator loaded" + gradeCalculatorSection.style.display);
-    } else {
-      gradeCalculatorSection.style.display = "none";
-    }
-  }
-  gradeCalculator.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent the default anchor action
-    toggleGradeCalculator();
-  });
-  // Function to add a grade input
+
   function addGradeInput() {
+    const gradeWrapper = document.createElement("div");
     const gradeInput = document.createElement("input");
     gradeInput.type = "number";
     gradeInput.placeholder = "Grade";
-    gradeInput.min = 1; // Set minimum grade to 1
-    gradeInput.max = 7; // Set maximum grade to 7
-    gradeInput.value = 1;
+    gradeInput.min = 1;
+    gradeInput.max = 7;
     gradeInput.className = "grade-input";
 
     const weightInput = document.createElement("input");
@@ -82,17 +67,15 @@ document.addEventListener("DOMContentLoaded", () => {
     weightDescription.textContent = "Weight: 1 = 100%, 0.5 = 50%, 2 = 200%";
     weightDescription.style.display = "block";
 
-    const gradeWrapper = document.createElement("div");
     gradeWrapper.appendChild(gradeInput);
     gradeWrapper.appendChild(weightInput);
-    gradesContainer.appendChild(gradeWrapper);
     gradeWrapper.appendChild(weightDescription);
+    gradesContainer.appendChild(gradeWrapper);
 
     gradeInput.addEventListener("input", updateAverage);
     weightInput.addEventListener("input", updateAverage);
   }
 
-  // Create 'Add Grade' button
   const addGradeButton = document.createElement("button");
   addGradeButton.textContent = "Add Grade";
   addGradeButton.addEventListener("click", addGradeInput);
@@ -103,17 +86,13 @@ document.addEventListener("DOMContentLoaded", () => {
   deleteGradeButton.addEventListener("click", deleteLastGrade);
   gradeCalculatorSection.appendChild(deleteGradeButton);
 
-  // Create average display
   const averageDisplay = document.createElement("div");
-  averageDisplay.innerHTML =
-    '<strong>Average Grade:</strong> <span id="average-grade">0</span>';
+  averageDisplay.innerHTML = '<strong>Average Grade:</strong> <span id="average-grade">0</span>';
   gradeCalculatorSection.appendChild(averageDisplay);
 
   const medianDisplay = document.createElement("div");
-  medianDisplay.innerHTML =
-    '<strong>Median Grade:</strong> <span id="median-grade">0</span>';
+  medianDisplay.innerHTML = '<strong>Median Grade:</strong> <span id="median-grade">0</span>';
   gradeCalculatorSection.appendChild(medianDisplay);
 
-  // Initialize with one grade input
   addGradeInput();
 });
--- navigation.js
+++ navigation.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function to show only the selected section
-    function showOnlySelectedSection(selectedSection) {
-      const sections = [librarySection, gradeCalculatorSection, timetableSection];
-      sections.forEach(section => {
-        section.style.display = (section === selectedSection) ? "block" : "none";
-      });
-    }
-  
-    const librarySection = document.getElementById("library");
-    const gradeCalculatorSection = document.getElementById("gradeCalculator");
-    const timetableSection = document.getElementById("timetable");
-  
-    librarySection.style.display = "none";
-    gradeCalculatorSection.style.display = "none";
-    timetableSection.style.display = "none";
-  
-    document.getElementById("library-link").addEventListener("click", (event) => {
-      event.preventDefault();
-      showOnlySelectedSection(librarySection);
+  function showOnlySelectedSection(selectedSection) {
+    const sections = [librarySection, gradeCalculatorSection, timetableSection];
+    sections.forEach(section => {
+      section.style.display = (section === selectedSection) ? "block" : "none";
     });
-  
-    document.getElementById("gradeCalculator-link").addEventListener("click", (event) => {
-      event.preventDefault();
-      showOnlySelectedSection(gradeCalculatorSection);
-    });
-  
-    document.getElementById("timetable-link").addEventListener("click", (event) => {
-      event.preventDefault();
-      showOnlySelectedSection(timetableSection);
-    });
+  }
+
+  const librarySection = document.getElementById("library");
+  const gradeCalculatorSection = document.getElementById("gradeCalculator");
+  const timetableSection = document.getElementById("timetable");
+
+  librarySection.style.display = "none";
+  gradeCalculatorSection.style.display = "none";
+  timetableSection.style.display = "none";
+
+  function handleNavLinkClick(event, section) {
+    event.preventDefault();
+    showOnlySelectedSection(section);
+  }
+
+  document.getElementById("library-link").addEventListener("click", (event) => {
+    handleNavLinkClick(event, librarySection);
   });
-  
+
+  document.getElementById("gradeCalculator-link").addEventListener("click", (event) => {
+    handleNavLinkClick(event, gradeCalculatorSection);
+  });
+
+  document.getElementById("timetable-link").addEventListener("click", (event) => {
+    handleNavLinkClick(event, timetableSection);
+  });
+});
